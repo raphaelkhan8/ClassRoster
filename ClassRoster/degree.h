2b89478ad2d5b692cef44d80d8f3d8fd32362616
Raphael Khan <raphaelkhan8@gmail.com>
2020-12-05 13:51:20 -0600

    defined enumerater for degree programs

---
+++ ClassRoster/degree.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+/* C. Define an enumerated data type DegreeProgram for the degree programs 
+      containing the data type values SECURITY, NETWORK, and SOFTWARE. */
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
