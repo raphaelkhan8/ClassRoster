485db7ca4e9407b267ec4cdfb7b2644ae178cd49
Raphael Khan <raphaelkhan8@gmail.com>
2020-12-06 11:57:22 -0600

    implemented Roster methods

--- ClassRoster/roster.cpp
+++ ClassRoster/roster.cpp
@@ -1,4 +1,5 @@
 #include "roster.h";
+#include "student.h";
 
 // E3a-E3f: implementation of Roster methods
 
@@ -70,9 +71,9 @@ void Roster::add(string studentID, string firstName, string lastName, string ema
 
 // E3b: implement remove method (removes the student object with the passed in studentID)
 void Roster::remove(string studentID) {
-	// default varable to false (id is not found)
+	// default varable to false (assume id is not found)
 	bool idFound = false;
-	for (int i = 0; i < numberOfStudents; i++) {
+	for (int i = 0; i < Roster::lastIndex; i++) {
 		// if id is found, set idFound to true and make the corresponding Student object disappear:
 		if (classRosterArray[i]->getID() == studentID) {
 			idFound = true;
@@ -87,4 +88,73 @@ void Roster::remove(string studentID) {
 			Roster::lastIndex--;
 		}
 	}
+	// print success message and the rest of the Students after student object is "removed"
+	if (idFound) {
+		cout << studentID << " was removed from the roster." << std::endl << std::endl;
+		this->printAll();
+	}
+	// else (if studentID was not found), print an error message
+	else {
+		cout << studentID << " was not found." << std::endl << std::endl;
+	}
+}
+
+// E3c: implement printAll method (prints a complete tab-separated list of student data for all students)
+void Roster::printAll() {
+	// print the Header
+	classRosterArray[0]->printHeader();
+	// loop through roster array and use Student's print method to print each student's info
+	for (int i = 0; i < Roster::lastIndex; i++) {
+		classRosterArray[i]->print();
+	}
+}
+
+// E3d: implement printAverageDaysInCourse method (prints student's average number of days in all three courses)
+void Roster::printAverageDaysInCourse(string studentID) {
+	bool idFound = false;
+	for (int i = 0; i < Roster::lastIndex; i++) {
+		// if id is found, set idFound to true and make the corresponding Student object disappear:
+		if (classRosterArray[i]->getID() == studentID) {
+			idFound = true;
+			cout << studentID << " (Average Days Per Course): ";
+			cout << (classRosterArray[i]->getCourseDurations()[0] +
+				classRosterArray[i]->getCourseDurations()[1] +
+				classRosterArray[i]->getCourseDurations()[2])
+				/ 3 << std::endl;
+		}
+	}
+	if (!idFound) cout << "Student ID not found :(" << std::endl;
+}
+
+// E3e: implement printInvalidEmails method (verifies student emails and prints all invalid emails)
+// All emails should include an at ('@') sign and period '.' and should not include any spaces.
+void Roster::printInvalidEmails() {
+	// assume there are no invalid emails
+	bool invalidEmails = false;
+	// loop through classRosterArray and get emails
+	for (int i = 0; i < Roster::lastIndex; i++) {
+		string email = (classRosterArray[i]->getEmail());
+		// if a space is found OR the email doesn't contain "@" OR the email doesn't contain "."
+		if (std::getline(std::cin, email) || email.find('@') == string::npos || email.find('.') == string::npos) {
+			// set invaledEmails to true and print the invalid email and their corresponding name
+			invalidEmails = true;
+			cout << email << ": " << classRosterArray[i]->getFullName() << std::endl;
+		}
+	}
+	// print message if no invalid emails are found
+	if (!invalidEmails) cout << "No invalid emails found :D" << std::endl;
+}
+
+// E3f: implemt printByDegreeProgram method (prints out student info for a degree program specified by degree program
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram) {
+	//print header
+	classRosterArray[0]->printHeader();
+	// loop through classRosterArray and look for student's whose degreeProgram matches passed in value
+	for (int i = 0; i < Roster::lastIndex; i++) {
+		// if degreeProgram's match, print the student's info
+		if (Roster::classRosterArray[i]->getDegreeProgram() == degreeProgram) {
+			classRosterArray[i]->print();
+		}
+	}
+	cout << std::endl;
 }
